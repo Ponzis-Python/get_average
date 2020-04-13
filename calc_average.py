fe54a932b39b20adbd38408e11d193a1140455a9
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2020-04-13 15:17:27 -0400

    Update calc_average.py

--- calc_average.py
+++ calc_average.py
@@ -6,5 +6,4 @@ num = int(input('How many numbers: '))
 for n in range(num):
     numbers = int(input('Enter number '))
     lst.append(numbers)
-print("Sum of elements in given list is :", sum(lst))
 print("The average of the numbers is:", sum(lst) / num)
