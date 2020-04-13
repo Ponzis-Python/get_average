ae729135735cdc60249a76bd0445955b0cff6a10
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2020-04-13 15:16:19 -0400

    Add files via upload

---
+++ calc_average.py
@@ -0,0 +1,10 @@
+#based off calculate sum code found here
+#https://medium.com/programminginpython-com/python-program-to-calculate-the-sum-of-elements-in-a-list-ed2b80db8268
+
+lst = []
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("Sum of elements in given list is :", sum(lst))
+print("The average of the numbers is:", sum(lst) / num)
